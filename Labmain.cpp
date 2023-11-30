e562da6daf123502addd62fccfa793398b9a8801
aalvaroo7 <almarrom@myuax.com>
2023-11-30 12:26:08 +0100

    comit gestion de errores no existe el archivo

--- Labmain.cpp
+++ Labmain.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <string>
 #include <cstdio>
-#include <cerrno> // Para usar errno
-
-using namespace std;
+#include <cstring>
+#include <stdexcept>
 
 struct ColorConsole
 {
@@ -14,54 +13,65 @@ struct ColorConsole
 struct ConsoleBox
 {
     void new_text() {/*...*/}
-    void set_text(const string &text) { cout << text << endl; }
+    void set_text(const std::string &text) { std::cout << text << std::endl; }
 };
 
-ConsoleBox *consoleBox = new ConsoleBox; // Suponemos que ya está inicializado
+ConsoleBox* consoleBox = new ConsoleBox; // Suponemos que ya está inicializado
 
 void load_script(const char* filename, bool show_script = false)
 {
-    string script;
-    FILE* f = nullptr;
+    std::string script;
+    FILE* f = fopen(filename, "rb");
+
+    if (!f)
+    {
+        std::cerr << "Error de apertura de " << filename << ": " << strerror(errno) << std::endl;
+        return;
+    }
+
     try
     {
-        f = fopen(filename, "rb");
-        if (!f)
-        {
-            cerr << "Error de apertura de " << filename << ": " << strerror(errno) << endl;
-            return;
-        }
-
-        int c;
         char buf[4001];
-        while ((c = fread(buf, 1, 4000, f)) > 0)
+        size_t bytesRead;
+
+        while ((bytesRead = fread(buf, 1, sizeof(buf) - 1, f)) > 0)
         {
-            buf[c] = 0;
+            buf[bytesRead] = '\0';
             script.append(buf);
         }
-        fclose(f);
-        f = nullptr;
 
         if (show_script)
         {
-            cout << ColorConsole::fg_blue << ColorConsole::bg_white;
-            cout << script << endl;
+            std::cout << ColorConsole::fg_blue << ColorConsole::bg_white;
+            std::cout << script << std::endl;
         }
+
         consoleBox->new_text();
         consoleBox->set_text(script);
     }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
     catch (...)
     {
-        cerr << "Error durante la lectura del archivo" << endl;
-        if(f)
-            fclose(f);
+        std::cerr << "Error desconocido durante la lectura del archivo" << std::endl;
     }
+
+    if (f)
+        fclose(f);
 }
 
 void load_script()
 {
     char filename[500];
-    printf("Archivo: ");
-    scanf("%499s", filename);
+    std::cout << "Archivo: ";
+    std::cin >> filename;
     load_script(filename, true);
+}
+
+int main()
+{
+    load_script();
+    return 0;
 }
